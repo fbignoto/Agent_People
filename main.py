0265b36bf46a4e6b770cfa255896b166a505a0b2
felipe.bignoto <felipe.bignoto@open-co.com>
2025-05-24 09:07:06 -0300

    Criando estrutura inicial do projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a + b
+
+def main():
+    print("Starting the program...")
+    result = soma(2, 5)
+    print(f"The result of the sum is: {result}")
+    print("Program finished.")
+
+# if __name__ == "__main__":
+#     main()
